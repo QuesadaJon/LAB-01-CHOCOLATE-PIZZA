ba7f3675e7215a273cf3fda53bbc13420532a590
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-10-28 13:29:50 -0700

    final

--- src/chocolate-pizza-js/Ingredients.js
+++ src/chocolate-pizza-js/Ingredients.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react'
 import IngredientsListLeft from './IngredientsListLeft'
-import IngredientsListRight from './IngredientsListRight'
-import data from './data';
-import ingredients from './data';
+
+import { data } from './data';
+
 export default class Ingredients extends Component {
     render() {
-        return
-        <div className="list-background"> {
-            data.map(item => {
-                return <IngredientsListLeft
-                    one={item.amount} two={item.name} />
-            })
-        }
 
-
-
-            <div className="list-background">
-                <div className="ingredients-left">
-                    <IngredientsListLeft name={['milk', 'mascarpone', 'pink salt', 'Black Mission Figs', 'brown sugar', 'water']} amount={['1 1/2 cups', '1/2 cup', '1/2 tsp', '1 lb', '1/2 cup', '2-4 tbsp']} />
-                </div>
-                <div className="ingredients-right">
-                    <IngredientsListRight name={['heavy cream', 'granulated sugar', 'egg yolks', 'lemon, juiced', 'butter', 'honeyroasted pecans, roughly chopped']} amount={['1 1/2 cups', '1/3', '2', '1', '2 tbsp', '1 cup']} />
-                </div
+        return (
+            <div className="list-background"> {
+                data.map(item => {
+                    return <IngredientsListLeft
+                        one={item.amount} two={item.name} />
+                })
+            }
             </div>
         )
     }
