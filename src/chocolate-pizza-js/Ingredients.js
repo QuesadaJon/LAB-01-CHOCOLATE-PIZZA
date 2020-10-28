63c9686c6144e1548774bf5edbcca1f9076205b8
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-10-27 17:17:05 -0700

    ingredients

--- src/chocolate-pizza-js/Ingredients.js
+++ src/chocolate-pizza-js/Ingredients.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import IngredientsListLeft from './IngredientsListLeft'
 import IngredientsListRight from './IngredientsListRight'
-
+import data from './data';
+import ingredients from './data';
 export default class Ingredients extends Component {
     render() {
         return (
-            <div className="list-background">
-                <IngredientsListLeft/>
-                <IngredientsListRight/>
+            <div className="list-background"> {
+                data.map(item => {
+                    return <IngredientsListLeft
+                        one={item.amount} two={item.name} />
+                })
+            }
+
+
             </div>
         )
     }
--- src/chocolate-pizza-js/IngredientsListLeft.js
+++ src/chocolate-pizza-js/IngredientsListLeft.js
@@ -5,13 +5,8 @@ export default class IngredientsList extends Component {
     render() {
         return (
             <div>
-                <p> {this.props.data.amount[0]} {this.props.data.name[0]}</p>
-                <p> {this.props.data.amount[1]} {this.props.data.name[1]}</p>
-                <p> {this.props.data.amount[2]} {this.props.data.name[2]}</p>
-                <p> {this.props.data.amount[3]} {this.props.data.name[3]}</p>
-                <p> {this.props.data.amount[4]} {this.props.data.name[4]}</p>
-                <p> {this.props.data.amount[5]} {this.props.data.name[5]}</p>
-                <p> {this.props.data.amount[6]} {this.props.data.name[6]}</p>
+                <p> {this.props.one} {this.props.two}</p>
+
             </div>
         )
     }
